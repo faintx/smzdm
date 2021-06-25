1b7099d97c68d4fe758237737e699aa4a88f1423
xiaokexiang <xxiaokexiang@gmail.com>
2021-06-25 16:28:35 +0800

    代码编写

---
+++ README.md
@@ -0,0 +1,30 @@
+# Bilibili动态推送脚本
+
+---
+<p style="text-align: center">
+    <img src="https://img.shields.io/badge/create-2021.04.20-brightgreen" alt="2021.06.23"/>
+    <img src="https://img.shields.io/badge/nodejs-15.4.0-red" alt="nodejs-15.4.0"/>
+    <img src="https://img.shields.io/badge/github%20-workflow-orange" alt="github action"/>
+    <img src="https://img.shields.io/badge/License-GPL-yellow" alt="GPL"/>
+</p>
+
+
+## 特别声明
+<b>此脚本只用于学习、测试使用，请勿将此项目的任何内容用于商业或非法目的！本人概不负责！</b>
+
+## 特别感谢
+- <b>@blackmatrix7</b>
+- <b>@wangfei021325</b>
+- <b>@chavyleung</b>
+
+### 推送参数配置
+
+Fork [此项目](https://github.com/xiaokexiang/smzdm) ，并添加如下`Secret`到<b>`Settings -> Secrets`</b>中：
+
+|    参数     |                             作用                             |
+| :---------: | :----------------------------------------------------------: |
+|   CORP_ID   | <a href='https://work.weixin.qq.com/wework_admin/frame#profile'>我的企业ID</a> |
+| CORP_SECRET |                  上步创建的应用详情界面可查                  |
+|   TO_USER   |         通讯录界面可查（可为`@all`全部用户都会收到）         |
+|  AGENT_ID   |                  上步创建的应用详情界面可查                  |
+|  COOKIE   |                  访问<a href="https://www.smzdm.com/">SMZDM</a>复制cookie                  |
---
+++ util/wechat.js
@@ -0,0 +1,64 @@
+const { MagicJS } = require('./magicJs.js');
+const scriptName = '什么值得买签到'
+let magicJS = MagicJS(scriptName, "INFO");
+
+function getTime() {
+    const now = new Date();
+    return '签到时间：' + now.getFullYear() + '年' + (now.getMonth() + 1) + '月' + now.getDate() + '日' + now.getHours() + '时' + now.getMinutes() + '分';
+}
+
+function getPushBody(title, subTile, content, params) {
+    // + !subTile || subTile == '' ? '' : (subTile + '\n') 
+    desc = '<div class=\"gray\">' + getTime() + '</div><div class=\"normal\">' + '</div><div class=\"highlight\">' + content + '</div>';
+    return JSON.stringify({
+        "chatid": "CHATID",
+        "msgtype": "textcard",
+        "touser": params.toUser,
+        "agentid": params.agentId,
+        "textcard": {
+            "title": title,
+            "description": desc,
+            "url": "https://www.smzdm.com/",
+            "btntxt": "更多"
+        },
+        "safe": 0
+    })
+}
+
+function sendMsg(title, subTile, content, params) {
+    console.log('params: ' + params)
+    corpid = 'ww74c7200c13352a24'
+    corpsecret = 'pT1Az_S6L5SIyRlBMr3_rki_V2kp45dvbq6QCLT1Uk8'
+    let accesTokenOption = {
+        url: `https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid=${params.corpid}&corpsecret=${params.corpsecret}`,
+    }
+    magicJS.get(accesTokenOption, (err, resp, data) => {
+        try {
+            if (JSON.parse(data).errcode !== 0) {
+                errmsg = JSON.parse(data).errmsg
+                magicJS.logWarning('获取企业微信推送access_token异常' + errmsg);
+                magicJS.notify(errmsg);
+            }
+            push(JSON.parse(data).access_token, getPushBody(title, subTile, content, params))
+        } catch (err) {
+            magicJS.logError(`请求access_token出现异常：${err}`);
+        }
+    })
+}
+
+function push(access_token, b) {
+    let pushOption = {
+        url: `https://qyapi.weixin.qq.com/cgi-bin/message/send?access_token=${access_token}`,
+        header: {
+            'Content-Type': 'application/json'
+        },
+        body: b
+    }
+    magicJS.post(pushOption, (err, resp, data) => {
+        console.log(err);
+        if (JSON.parse(data).errcode == 0) {
+            magicJS.notify('发送消息成功！', '', data)
+        }
+    })
+}
+module.exports = { sendMsg }
